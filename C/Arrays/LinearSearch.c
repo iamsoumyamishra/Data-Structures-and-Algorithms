2bc96450fba970272724b285bd12613ff5f056f0
iamsoumyamishra <soumyamishra2099@gmail.com>
2026-02-07 16:14:21 +0530

    UPDATE: Linear Search

--- C/Arrays/LinearSearch.c
+++ C/Arrays/LinearSearch.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main() {
 
-    int n, key, flag = 0, index;
+    int n, target, flag = 0, index;
     scanf("%d", &n);
 
     int * arr = (int*) malloc(n * sizeof(int));
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
+
         scanf("%d", &arr[i]);
     }
 
-    scanf("%d", &key);
+    scanf("%d", &target);
 
-    for(int i = 0; i < n; i++){
-
-        if (arr[i] == key){
-            index = i;
+    for (int i = 0; i < n; i++) {
+        if (target == arr[i]){
             flag = 1;
+            index = i;
             break;
         }
     }
-    
-    if (flag) {
-        printf("%d found at Index %d\n", key, index);
-    } else {
-        printf("%d not found\n", key);
-    }
 
+    if (flag == 1) {
+        printf("%d found at index at %d\n", target, index);
+    } else {
+        printf("%d not found\n", target);
+    }
+    
     return 0;
 }
